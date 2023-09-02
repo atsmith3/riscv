4ddfdecc8ba2f57298c3a5bd2f92b901422be50e
Andrew Smith <atsmith19@comcast.net>
2023-09-02 12:42:23 -0700

    Fixing parameters in memory.sv

--- rtl/memory.sv
+++ rtl/memory.sv
@@ -3,7 +3,7 @@
  * Single port memory model for testing.
  */
 
-module mem #(parameter SIZE=4294967296, parameter DEPTH=8, parameter W_DELAY=10, parameter R_DELAY=10, parameter ) ();
+module mem #(parameter SIZE=65536, parameter DEPTH=8, parameter W_DELAY=10, parameter R_DELAY=10) ();
 
   reg [SIZE-1:0][DEPTH-1:0] data;
 
