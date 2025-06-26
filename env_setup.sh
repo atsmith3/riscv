28207cdca64155cb73bf0b5a7724f9a7c59db2c5
Andrew Smith <atsmith19@comcast.net>
2025-06-26 07:48:01 -0700

    Chore/precommit (#5)
    * Adding pre-commit basic hooks
    
    * Adding pre commit hoooks for c and python code
    
    * C++ hook working
    
    * Updating path to slang-verilog

--- env_setup.sh
+++ env_setup.sh
@@ -2,4 +2,4 @@
 
 export WORKSPACE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${WORKSPACE}/.venv/bin/activate
-export PATH="/home/andrew/software/slang/slang-7.0/build/bin:$PATH"
+export PATH="/home/andrew/software/slang/build/bin:$PATH"
--- test/gcd/gcd.c
+++ test/gcd/gcd.c
@@ -4,20 +4,19 @@
 #endif
 
 int gcd(int a, int b) {
-#ifdef LOCAL_COMPILE 
+#ifdef LOCAL_COMPILE
   int i = 0;
 #endif
   while (a != b) {
-#ifdef LOCAL_COMPILE 
-    printf("i: %d, a: %d, b: %d\n",i,a,b);
+#ifdef LOCAL_COMPILE
+    printf("i: %d, a: %d, b: %d\n", i, a, b);
 #endif
     if (a > b) {
       a = a - b;
-    }
-    else {
+    } else {
       b = b - a;
     }
-#ifdef LOCAL_COMPILE 
+#ifdef LOCAL_COMPILE
     i += 1;
 #endif
   }
@@ -25,13 +24,14 @@ int gcd(int a, int b) {
 }
 
 int main() {
-  //int result = gcd(272,1479); // 17
-  int result = gcd(1479,272); // 17
-  //int result = gcd(22,26); // 2
-  //int result = gcd(45,40); // 5
-  //int result = gcd(6,4); // 2
-  if(result == 17) {
-    while(1);
+  // int result = gcd(272,1479); // 17
+  int result = gcd(1479, 272); // 17
+  // int result = gcd(22,26); // 2
+  // int result = gcd(45,40); // 5
+  // int result = gcd(6,4); // 2
+  if (result == 17) {
+    while (1)
+      ;
   }
   return 0;
 }
